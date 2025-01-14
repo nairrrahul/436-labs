f62893a4daf5870982319aa352ca3a16c4b7863b
Rahul Nair <nair.rahul992@gmail.com>
2025-01-13 22:05:42 -0800

    init preview

---
+++ Lab1/app/src/main/java/com/example/graduationrsvp/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.graduationrsvp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.graduationrsvp.ui.theme.GraduationRSVPTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GraduationRSVPTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GradScreen()
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun GradScreen(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(R.drawable.grad_cap),
+        contentDescription = "Graduation cap",
+        alpha = 0.3f
+    )
+    Text(
+        text = stringResource(R.string.graduation_announcement),
+        fontSize = 50.sp,
+        color = Color.Red,
+        modifier = modifier,
+        textAlign = TextAlign.Center,
+        fontWeight = FontWeight.Bold,
+        lineHeight = 50.sp
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GradPreview() {
+    GraduationRSVPTheme {
+        GradScreen()
+    }
+}
