394c84a8132154813c53f6fed96e36c0e1f416b4
Rahul Nair <nair.rahul992@gmail.com>
2025-01-13 22:21:36 -0800

    Without Preview + Alignment, Icon, Chapter 2 completion

--- Lab1/app/src/main/java/com/example/graduationrsvp/MainActivity.kt
+++ Lab1/app/src/main/java/com/example/graduationrsvp/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -36,23 +39,27 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview
 @Composable
 fun GradScreen(modifier: Modifier = Modifier) {
-    Image(
-        painter = painterResource(R.drawable.grad_cap),
-        contentDescription = "Graduation cap",
-        alpha = 0.3f
-    )
-    Text(
-        text = stringResource(R.string.graduation_announcement),
-        fontSize = 50.sp,
-        color = Color.Red,
-        modifier = modifier,
-        textAlign = TextAlign.Center,
-        fontWeight = FontWeight.Bold,
-        lineHeight = 50.sp
-    )
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Image(
+            painter = painterResource(R.drawable.grad_cap),
+            contentDescription = "Graduation cap",
+            alpha = 0.3f
+        )
+        Text(
+            text = stringResource(R.string.graduation_announcement),
+            fontSize = 50.sp,
+            color = Color.Red,
+            modifier = modifier,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold,
+            lineHeight = 50.sp
+        )
+    }
 }
 
 @Preview(showBackground = true)
